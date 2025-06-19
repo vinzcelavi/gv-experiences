0942c43de00aa43e76ce2a8317da1624b7ba3b6f
Vincent Bianciotto <vincent@celavi.fr>
2025-06-19 18:51:18 +0200

    Use Gsap

--- src/components/CountryItem.js
+++ src/components/CountryItem.js
@@ -1,4 +1,6 @@
-// Utility functions similar to Codrops
+import { gsap } from 'gsap';
+
+// Utility functions from Codrops
 const map = (value, in_min, in_max, out_min, out_max) => {
   return ((value - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 };
@@ -36,6 +38,14 @@ export default class CountryItem {
     // the item text
     this.DOM.textInner = this.DOM.el.querySelector('a');
     
+    // menu item properties that will animate as we move the mouse around the menu
+    this.animatableProperties = {
+      tx: { previous: 0, current: 0, amt: 0.08 },
+      ty: { previous: 0, current: 0, amt: 0.08 },
+      rotation: { previous: 0, current: 0, amt: 0.08 },
+      brightness: { previous: 1, current: 1, amt: 0.08 }
+    };
+    
     // create the image structure
     this.layout();
     
@@ -51,7 +61,7 @@ export default class CountryItem {
   //   </div>
   // </div>
   layout() {
-    // this is the element that gets its position animated
+    // this is the element that gets its position animated (and perhaps other properties like the rotation etc..)
     this.DOM.reveal = document.createElement('div');
     this.DOM.reveal.className = 'hover-reveal';
     
@@ -100,19 +110,54 @@ export default class CountryItem {
 
   // show the image element
   showImage() {
-    // set initial styles
-    this.DOM.reveal.style.opacity = 1;
-    this.DOM.el.style.zIndex = 1000;
+    // kill any current tweens
+    gsap.killTweensOf(this.DOM.revealInner);
+    gsap.killTweensOf(this.DOM.revealImage);
     
+    this.tl = gsap.timeline({
+      onStart: () => {
+        // show the image element
+        this.DOM.reveal.style.opacity = 1;
+        // set a high z-index value so image appears on top of other elements
+        gsap.set(this.DOM.el, { zIndex: 1000 });
+      }
+    })
     // animate the image wrap
-    this.DOM.revealInner.style.transform = 'translateX(0%)';
-    this.DOM.revealImage.style.transform = 'translateX(0%)';
+    .to(this.DOM.revealInner, 0.2, {
+      ease: 'Sine.easeOut',
+      startAt: { x: direction.x < 0 ? '-100%' : '100%' },
+      x: '0%'
+    })
+    // animate the image element
+    .to(this.DOM.revealImage, 0.2, {
+      ease: 'Sine.easeOut',
+      startAt: { x: direction.x < 0 ? '100%' : '-100%' },
+      x: '0%'
+    }, 0);
   }
 
   // hide the image element
   hideImage() {
-    this.DOM.el.style.zIndex = 10;
-    this.DOM.reveal.style.opacity = 0;
+    // kill any current tweens
+    gsap.killTweensOf(this.DOM.revealInner);
+    gsap.killTweensOf(this.DOM.revealImage);
+    
+    this.tl = gsap.timeline({
+      onStart: () => {
+        gsap.set(this.DOM.el, { zIndex: 1 });
+      },
+      onComplete: () => {
+        gsap.set(this.DOM.reveal, { opacity: 0 });
+      }
+    })
+    .to(this.DOM.revealInner, 0.2, {
+      ease: 'Sine.easeOut',
+      x: direction.x < 0 ? '100%' : '-100%'
+    })
+    .to(this.DOM.revealImage, 0.2, {
+      ease: 'Sine.easeOut',
+      x: direction.x < 0 ? '-100%' : '100%'
+    }, 0);
   }
 
   // start the render loop animation (rAF)
@@ -150,20 +195,29 @@ export default class CountryItem {
     
     // new translation values
     // the center of the image element is positioned where the mouse is
-    const tx = Math.abs(mousepos.x - this.bounds.el.left) - this.bounds.reveal.width / 2;
-    const ty = Math.abs(mousepos.y - this.bounds.el.top) - this.bounds.reveal.height / 2;
+    this.animatableProperties.tx.current = (mousepos.x - this.bounds.el.left / 2) - this.bounds.reveal.width / 2;
+    this.animatableProperties.ty.current = (mousepos.y - this.bounds.el.top / 2) - this.bounds.reveal.height / 2;
     
     // new rotation value
-    // const rotation = this.firstRAFCycle ? 0 : map(mouseDistanceX, 0, 100, 0, direction.x < 0 ? 60 : -60);
-    const rotation = 0;
+    this.animatableProperties.rotation.current = this.firstRAFCycle ? 0 : map(mouseDistanceX, 0, 100, 0, direction.x < 0 ? 60 : -60);
     
     // new filter value
-    // const brightness = this.firstRAFCycle ? 1 : map(mouseDistanceX, 0, 100, 1, 4);
-    const brightness = 1;
+    this.animatableProperties.brightness.current = this.firstRAFCycle ? 1 : map(mouseDistanceX, 0, 100, 1, 4);
+    
+    // set up the interpolated values
+    // for the first cycle, both the interpolated values need to be the same so there's no "lerped" animation between the previous and current state
+    this.animatableProperties.tx.previous = this.firstRAFCycle ? this.animatableProperties.tx.current : lerp(this.animatableProperties.tx.previous, this.animatableProperties.tx.current, this.animatableProperties.tx.amt);
+    this.animatableProperties.ty.previous = this.firstRAFCycle ? this.animatableProperties.ty.current : lerp(this.animatableProperties.ty.previous, this.animatableProperties.ty.current, this.animatableProperties.ty.amt);
+    this.animatableProperties.rotation.previous = this.firstRAFCycle ? this.animatableProperties.rotation.current : lerp(this.animatableProperties.rotation.previous, this.animatableProperties.rotation.current, this.animatableProperties.rotation.amt);
+    this.animatableProperties.brightness.previous = this.firstRAFCycle ? this.animatableProperties.brightness.current : lerp(this.animatableProperties.brightness.previous, this.animatableProperties.brightness.current, this.animatableProperties.brightness.amt);
     
     // set styles
-    this.DOM.reveal.style.transform = `translate3d(${tx}px, ${ty}px, 0) rotate(${rotation}deg)`;
-    this.DOM.reveal.style.filter = `brightness(${brightness})`;
+    gsap.set(this.DOM.reveal, {
+      x: this.animatableProperties.tx.previous,
+      y: this.animatableProperties.ty.previous,
+      rotation: this.animatableProperties.rotation.previous,
+      filter: `brightness(${this.animatableProperties.brightness.previous})`
+    });
     
     // loop
     this.firstRAFCycle = false;
